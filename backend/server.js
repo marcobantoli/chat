e3a64bd804be24e2cf6f7edf821d4a2ede4d105c
Marco Bantolino <marcobantoli@gmail.com>
2022-12-26 12:30:00 -0500

    Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
